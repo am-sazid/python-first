5a187798e6cf5ec79be23f6376dcd83886623c89
am-sazid <141324884+am-sazid@users.noreply.github.com>
2023-08-13 21:30:42 +0600

    new code

--- list data type.py
+++ list data type.py
@@ -1,3 +1,4 @@
+'''
 # Python List Data Type
      # int type list
 li = [12,13,23,43,54,65,76]
@@ -20,3 +21,37 @@ print(lis)
 list= [False, True, False, True]
 
 print (type(list))
+
+
+# Access List Item
+
+sazid = ["facebook", "group", "website", "page" ]
+
+print(sazid[2])
+
+# change list Item
+
+hablu = ["facebook", "group", "website", "page" ]
+
+hablu[2]= "massenger"
+
+print(hablu)
+
+
+# ADD List Item
+    #Append 
+    
+hablu = ["facebook", "group", "website", "page" ]
+
+hablu.append("my page")
+print(hablu)
+
+
+    # Insert
+
+hablu = ["facebook", "group", "website", "page" ]
+
+hablu.insert(4, " page")
+
+print(hablu)
+'''
