926bf09e95d4ca3ae312b8137d7fa355af4658e4
am-sazid <141324884+am-sazid@users.noreply.github.com>
2023-08-16 21:20:58 +0600

    new added

---
+++ List data type.py
@@ -0,0 +1,136 @@
+
+# Python List Data Type
+     # int type list
+li = [12,13,23,43,54,65,76]
+
+print(li)
+
+li[1]=0
+
+print(li)
+
+    # string type list
+    
+lis = ["sazid " "khalid " "sifat " "arafat"]
+
+
+print(lis)
+
+    # boolean type list
+    
+list= [False, True, False, True]
+
+print (type(list))
+
+
+# Access List Item
+
+sazid = ["facebook", "group", "website", "page" ]
+
+print(sazid[2])
+
+# change list Item
+
+hablu = ["facebook", "group", "website", "page" ]
+
+hablu[2]= "massenger"
+
+print(hablu)
+
+
+# ADD List Item
+    #Append 
+    
+hablu = ["facebook", "group", "website", "page" ]
+
+hablu.append("my page")
+print(hablu)
+
+
+    # Insert
+
+hablu = ["facebook", "group", "website", "page" ]
+
+hablu.insert(4, " page")
+
+print(hablu)
+
+# The remove() method removes the specified item.
+
+Mylist = ["sazid", "saju", "rifat", "shikto"]
+
+Mylist.remove("sazid")
+
+print(Mylist)
+
+
+# The pop() method removes the specified index.
+
+Mylist = ["sazid", "saju", "rifat", "shikto"]
+
+Mylist.pop(2)
+
+print(Mylist)
+
+
+# The del keyword also removes the specified index:
+
+Mylist = ["sazid", "saju", "rifat", "shikto", 420]
+
+Mylist.pop()
+
+print(Mylist)
+
+# The del keyword also removes the specified index:
+
+Mylist = ["sazid", "saju", "rifat", "shikto", 420]
+
+del Mylist[4]
+
+print(Mylist)
+
+# Clear the List
+
+Mylist = ["sazid", "saju", "rifat", "shikto", 420]
+
+Mylist.clear()
+
+print(Mylist)
+
+# You can loop through the list items by using a for loop:
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+# Use the range() and len() functions to create a suitable iterable
+# index number output
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(i)
+  
+# Use the range() and len() functions to create a suitable iterable
+# output string
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+  
+
+# Print all items, using a while loop to go through all the index numbers
+# trying
+Mylist = ["sazid", "saju", "rifat", "shikto", 420]
+
+y= 0
+
+while y < len(Mylist):
+    print(Mylist[y])
+    y = y + 1
+ 
+# w3 school
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+  
---
+++ area shape calculate.py
@@ -0,0 +1,16 @@
+# Triangle
+base = float(input("enter base : "))
+height = float(input("enter height : "))
+
+area = 0.5 * base * height
+
+print("area of Triangle " ,area)
+
+# Circle 
+
+radius =float(input("enter radius"))
+
+area = 3.1416 * radius * radius
+
+
+print("area of circle " ,area)
